3488c1d81c5c25f5d46149713d495423d5ae4f33
merkwur <semihiyikalender@gmail.com>
2022-07-01 16:09:40 +0300

    Add files via upload

--- columnwise_intensity.c
+++ columnwise_intensity.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "headers/stb_image.h"
@@ -8,36 +7,36 @@
 #include "headers/stb_image_write.h"
 
 enum img_channels {
-    R, G, B, A
+    R, G, B
 };
 
 int main(void){
     int width, height, channels, pixel_index, c; 
     unsigned char *image = stbi_load("clock_0000.png", &width, &height, &channels, 0);
-    unsigned char histogram[256][width];
+
+    /* craete an array for the histogram */ 
+
+    unsigned char histogram[256][width][3]; 
     
     if(image == NULL){
-        printf("Error Image didn't laod");
+        printf("Error Image didn't load");
         exit(1);
     }
     
-    printf("Image successfully loaded (%d, %d, %d)", width, height, channels);
-    clock_t begin = clock();
+    printf("Image successfully loaded (%d, %d, %d)", height, width, channels);
+
     
-    c = 0;
     memset(histogram, 0, sizeof(histogram));
 
     for (int y = 0; y < height; y++){
         for (int x = 0; x < width; x++){
             pixel_index = (y * width + x) * channels;
-            histogram[255-image[pixel_index+R]][x] += 1;
+            histogram[255-image[pixel_index+R]][x][R] += 1;
+            histogram[255-image[pixel_index+G]][x][G] += 1;
+            histogram[255-image[pixel_index+B]][x][B] += 1;
         }
     }
 
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("%f ", time_spent);
-
-    stbi_write_png("hist.png", width, 256, 1, histogram, width);
+    stbi_write_png("hist.png", width, 256, 3, histogram, width * 3);
     stbi_image_free(image);
 }
